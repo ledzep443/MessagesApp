376de144614c905ac6efc9e2d2f6512bb80104e2
Jimmy <jimmy@purnellsoftwaredevelopment.com>
2022-04-10 06:49:29 -0400

    Added service dependencies to Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,8 @@
+using Blazored.LocalStorage;
 using Client;
+using Client.Service;
+using Client.Service.IService;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +11,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 await builder.Build().RunAsync();
